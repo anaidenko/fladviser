a71ccf2d5439977f6b26b8ca0ccb145744d30902
anaidenko <Andrii.Naidenko@outlook.com>
2015-07-13 03:22:02 +0200

    Restored travis node_env, added mongo env variable.

--- config/env/all.js
+++ config/env/all.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/fladviser',
 	app: {
 		title: 'fladviser',
 		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
